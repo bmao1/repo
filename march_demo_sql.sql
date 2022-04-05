b4dba04a33958509125744c9b91f849f3b4713fa
bmao1 <logttt@gmail.com>
2022-04-05 16:48:00 -0400

    Update march_demo_sql.sql

--- march_demo_sql.sql
+++ march_demo_sql.sql
@@ -399,6 +399,156 @@ group by cube(enct_date, symptoms, gender, age_group, race, covid_diagnosis, cov
 
 
 
+-- symptom 7.1
+
+
+
+with encounter as (
+    select distinct enct.reference_id_aa as encounter_id
+        , subject.reference_id_aa as subject_id
+        , min(substr("date", 1, 10)) as enct_date
+    from documentreference, unnest(context.encounter) t(enct)
+    group by enct.reference_id_aa, subject.reference_id_aa
+),
+
+
+symptom as (SELECT distinct subject.reference_id_aa as subject_id
+    , encounter.reference_id_aa as encounter_id
+    , codecoding.code as symptom_code
+    , codecoding.system as symptom_code_system
+    , codecoding.display_aa as display
+FROM "delta"."observation"
+    , unnest(modifierExtension) t(modext)
+    , unnest(code.coding) t(codecoding)
+where modext.url = 'http://fhir-registry.smarthealthit.org/StructureDefinition/nlp-polarity' and modext.valueinteger = 0
+    and codecoding.system = 'http://snomed.info/sct'
+    and codecoding.code in ('68235000','64531003','49727002','11833005','28743005','62315008','84229001','367391008','43724002','103001002','426000000','25064002','21522001','29857009','57676002','68962001','422587007','422400008','44169009','36955009','267036007','162397003')
+-- limit 5
+),
+
+fhir_patient as (SELECT DISTINCT
+    gender
+, "date"("concat"(birthdate, '-01-01')) dob
+, ("year"("now"()) - CAST(birthdate AS int)) age
+, (CASE WHEN (ext.valuecoding.code IN ('1002-5', '2028-9', '2054-5', '2076-8', '2106-3')) THEN ext.valuecoding.display_aa ELSE null END) race
+, addr.postalcode postalcode
+, id subject_id
+FROM
+  patient
+, UNNEST(extension) t (ext)
+, UNNEST(address) t (addr)
+WHERE ((birthdate IS NOT NULL) AND (gender IS NOT NULL))
+),
+
+dx as (SELECT distinct encounter.reference_id_aa as encounter_id 
+    , subject.reference_id_aa as subject_id
+    , onsetdatetime as enct_date
+    , 'dx U07.1' as test_method
+    , 'Positive' as covid_result
+FROM "delta"."condition" , unnest(code.coding) t(codecoding)
+where codecoding.code = 'U07.1'
+), 
+
+lab_test as (SELECT distinct subject.reference_id_aa as subject_id
+    , encounter.reference_id_aa as encounter_id
+    , effectiveDatetime as enct_date
+    , case when valuecode.system = 'http://snomed.info/sct' and valuecode.code = '260385009' then 'Negative'
+           when valuecode.system = 'http://snomed.info/sct' and valuecode.code = '10828004' then 'Positive'
+           end as covid_lab_test
+FROM "delta"."observation"
+    , unnest(valueCodeableConcept.coding) t(valuecode)
+    , unnest(code.coding) t(codecoding)
+    , unnest(category) t(categ)
+    , unnest(categ.coding) t(categcode)
+where modifierExtension is null 
+    and codecoding.code in ('94500-6','95406-5')
+    and categcode.code = 'laboratory'
+),
+
+classifier as (
+SELECT distinct subject.reference_id_aa as subject_id
+    , encounter.reference_id_aa as encounter_id 
+    , effectiveDatetime as enct_date
+    , case when valuecode.system = 'http://snomed.info/sct' and valuecode.code = '260385009' then 'Negative'
+           when valuecode.system = 'http://snomed.info/sct' and valuecode.code = '10828004' then 'Positive'
+           end as covid_classifier
+FROM "delta"."observation", unnest(modifierExtension) t(modext), unnest(valueCodeableConcept.coding) t(valuecode)
+where modext.url = 'http://fhir-registry.smarthealthit.org/StructureDefinition/nlp-classifier' and modext.valueInteger = 1
+),
+
+combine as (
+select distinct e.encounter_id
+    , e.subject_id
+    , date(date_trunc('month', date_parse(e.enct_date,'%Y-%m-%d')))  as enct_date 
+    , case symptom_code when '68235000' then 'Congestion or runny nose'
+                        when '64531003' then 'Congestion or runny nose'
+                        when '49727002' then 'Cough'
+                        when '11833005' then 'Cough'
+                        when '28743005' then 'Cough'
+                        when '62315008' then 'Diarrhea'
+                        when '84229001' then 'Fatigue'
+                        when '367391008' then 'Fatigue'
+                        when '43724002' then 'Fever or chills'
+                        when '103001002' then 'Fever or chills'
+                        when '426000000' then 'Fever or chills'
+                        when '25064002' then 'Headache'
+                        when '21522001' then 'Muscle or body aches'
+                        when '29857009' then 'Muscle or body aches'
+                        when '57676002' then 'Muscle or body aches'
+                        when '68962001' then 'Muscle or body aches'
+                        when '422587007' then 'Nausea or vomiting'
+                        when '422400008' then 'Nausea or vomiting'
+                        when '44169009' then 'New loss of taste or smell'
+                        when '36955009' then 'New loss of taste or smell'
+                        when '267036007' then 'Shortness of breath or difficulty breathing'
+                        when '162397003' then 'Sore throat'
+                        else 'Other Symptoms' end as symptoms
+    , case when p.gender in ('male', 'female') then gender else 'Missing' end as gender
+    , (CASE WHEN (p.age < 19) THEN '0-18' 
+            WHEN (p.age BETWEEN 19 AND 44) THEN '19-44' 
+            WHEN (p.age BETWEEN 45 AND 64) THEN '45-64' 
+            WHEN (p.age BETWEEN 65 AND 84) THEN '65-84' 
+            WHEN (p.age BETWEEN 85 AND 300) THEN '85+' ELSE 'Missing' END) as age_group
+    , case when p.race is not null then p.race else 'Missing' end as race
+    , case when p.postalcode is not null then p.postalcode else 'Missing' end as postalcode3dig
+    , case when dx.covid_result is not null then dx.covid_result else 'No covid diagnosis' end as covid_diagnosis
+    , case when l.covid_lab_test is not null then l.covid_lab_test else 'No lab test' end as covid_lab_test
+    , case when c.covid_classifier is not null then c.covid_classifier else 'No classifier' end as covid_classifier
+    
+from encounter e
+    left join 
+    symptom s
+    on e.encounter_id = s.encounter_id
+    left join 
+    fhir_patient p
+    on e.subject_id = p.subject_id
+    left join
+    dx
+    on e.encounter_id = dx.encounter_id
+    left join 
+    lab_test l
+    on e.encounter_id = l.encounter_id
+    left join 
+    classifier c
+    on e.encounter_id = c.encounter_id
+    
+)
+
+-- change in v7: count patients, not encounters
+select enct_date
+    , symptoms
+    , gender
+    , age_group
+    , race
+    , covid_diagnosis
+    , covid_lab_test
+    , covid_classifier
+    , count(distinct subject_id) as cnt
+from combine
+where year(enct_date) >= 2016 and enct_date <= now()
+group by cube(enct_date, symptoms, gender, age_group, race, covid_diagnosis, covid_lab_test, covid_classifier)
+
+
 
 
 
